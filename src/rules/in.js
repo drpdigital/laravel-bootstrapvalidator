cb3d47ada2ec640ea5f5820d43609a807c961a76
Stuart Lee <stuart.lee@drpgroup.com>
2014-07-22 17:52:04 +0100

    added in

---
+++ src/rules/in.js
@@ -0,0 +1,22 @@
+(function($) {
+
+    $.fn.bootstrapValidator.i18n.in = $.extend($.fn.bootstrapValidator.i18n.in || {}, {
+        'default' : 'The selected value is invalid'
+    });
+
+    $.fn.bootstrapValidator.validators.in = {
+
+        validate: function(validator, $field, options) {
+
+            var value = $field.val();
+
+            var message = options.message['in'] || options.message || $.fn.bootstrapValidator.i18n.in.default;
+
+            return {
+                valid : $.inArray(value, options.values) >= 0,
+                message : message
+            }
+        }
+    };
+
+}(window.jQuery));
